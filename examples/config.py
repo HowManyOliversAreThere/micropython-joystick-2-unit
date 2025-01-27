6acd91a861d15a6248d5d77bf6d6e884525396fc
Oliver Robson <oliver.robson.ecse@gmail.com>
2025-01-27 18:59:32 +1100

    Use SoftI2C to eradicate warning

--- examples/config.py
+++ examples/config.py
@@ -1,9 +1,9 @@
-from machine import I2C, Pin
+from machine import SoftI2C, Pin
 
 import joystick_2_unit
 
 SCL = 32
 SDA = 26
 
-i2c = I2C(scl=Pin(SCL), sda=Pin(SDA))
+i2c = SoftI2C(scl=Pin(SCL), sda=Pin(SDA))
 joystick = joystick_2_unit.Joystick2Unit(i2c)
